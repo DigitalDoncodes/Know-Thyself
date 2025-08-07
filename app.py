9a7da1f5b73c36aa86be6444f4488885c333460b
DigitalDoncodes <psychologyresumemail@gmail.com>
2025-08-07 11:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # Configure your SMTP settings
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] =587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
